3b357d0e95f8941bf5194631a74af9a9b8054477
TomayGo <76687431+TomayGo@users.noreply.github.com>
2024-11-07 11:37:34 +0900

    update package version

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 apt install -y proxmox-headers-$(uname -r) wget unzip dkms
 mkdir e810
 cd e810
-wget https://downloadmirror.intel.com/831137/Release_29.3.zip
-unzip Release_29.3.zip
+wget https://downloadmirror.intel.com/832330/Release_29.3.1.zip
+unzip Release_29.3.1.zip
 cd PROCGB/Linux
 tar xvf ice-1.15.4.tar.gz
 curl https://raw.githubusercontent.com/TomayGo/ice-dkms/main/dkms.conf -o ice-1.15.4/dkms.conf
